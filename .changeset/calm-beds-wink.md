3a8b05dfdb1c4adbb9fa91ef9b98d1176aa46615
Mikkel Laursen <mlaursen03@gmail.com>
2026-01-19 16:36:14 -0500

    feat(cli): add copy to root option

---
+++ .changeset/calm-beds-wink.md
@@ -0,0 +1,5 @@
+---
+"@mlaursen/cli": minor
+---
+
+Added the ability to also copy files to the root with the -r, --copy-to-root repeatable option
--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -3,11 +3,13 @@ import {
   copyScssFiles,
 } from "@mlaursen/copy-scss-files";
 import { Command } from "commander";
+import { basename, join } from "node:path";
 
-type ProgramCopyScssFilesOptions = Omit<
-  Required<CopyScssFilesOptions>,
-  "pattern" | "getDistPaths"
->;
+interface ProgramCopyScssFilesOptions extends Required<
+  Omit<CopyScssFilesOptions, "getDistPaths" | "pattern">
+> {
+  copyToRoot: Set<string>;
+}
 
 const program = new Command("@mlaursen/cli");
 program
@@ -16,10 +18,31 @@ program
   .option("--src <src>", "An optional src directory", "src")
   .option("-o, --out <dir>", "An optional dist directory", "dist")
   .option("-w, --watch", "Watch mode", false)
+  .option<Set<string>>(
+    "-r, --copy-to-root <fileName>",
+    "Also copy this file to the root directory",
+    (value, previous) => {
+      previous.add(value);
+      return previous;
+    },
+    new Set()
+  )
   .action((pattern: string, options: ProgramCopyScssFilesOptions) => {
+    const { copyToRoot, ...copyOptions } = options;
     return copyScssFiles({
-      ...options,
+      ...copyOptions,
       pattern,
+      getDistPaths: (path, renameDist) => {
+        const paths = [renameDist(path)];
+        if (copyToRoot.size > 0) {
+          const name = basename(path);
+          if (copyToRoot.has(name)) {
+            paths.push(renameDist(join(options.src, name)));
+          }
+        }
+
+        return paths;
+      },
     });
   });
 
