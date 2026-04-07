13ed2b14fc20a405b98bd8aa2faaca6310aa5f70
Mikkel Laursen <mlaursen03@gmail.com>
2026-04-07 17:01:09 -0400

    feat(scss): export additional files

--- packages/scss/src/index.ts
+++ packages/scss/src/index.ts
@@ -1,4 +1,6 @@
+export * from "./assertSassResolvePackageJson.js";
 export * from "./compileScss.js";
 export * from "./compileScssModule.js";
 export * from "./minifyCss.js";
+export * from "./constants.js";
 export * from "./utils.js";
