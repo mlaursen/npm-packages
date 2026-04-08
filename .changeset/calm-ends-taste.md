c39ef2fb656e37205e8faba590388020fab3517a
Mikkel Laursen <mlaursen03@gmail.com>
2026-04-08 16:48:33 -0400

    fix(eslint-config): fixed type definitions after new bundler
    fix eslint?

---
+++ .changeset/calm-ends-taste.md
@@ -0,0 +1,5 @@
+---
+"@mlaursen/eslint-config": patch
+---
+
+Fixed the type definitions after creating a smaller bundle
--- packages/eslint-config/eslint.config.mjs
+++ packages/eslint-config/eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from "eslint/config";
-import { configs, gitignore } from "./dist/index.mjs";
+import { configs, gitignore } from "./dist/index.js";
 
 export default defineConfig([
   gitignore(import.meta.url),
