bb8ba8ca8e33358e297cf6fae4ce4e2a288d8333
Mikkel Laursen <mlaursen03@gmail.com>
2026-05-26 15:48:00 -0400

    chore(website): fix njk formatting

--- apps/website/src/pages/packages/wc/wc.11tydata.js
+++ apps/website/src/pages/packages/wc/wc.11tydata.js
@@ -8,3 +8,34 @@ export const symbols = true;
 export const currentVersion = 0;
 
 export const sourcePath = "npm-packages/packages/wc";
+
+/** @type {import("@mlaursen/wc/types").DefaultComponentSize[]} */
+export const defaultSizes = ["small", "medium", "large"];
+
+/** @type {import("@mlaursen/wc/types").DefaultComponentExtraSize[]} */
+export const defaultExtraSizes = [
+  "extra-small",
+  ...defaultSizes,
+  "extra-large",
+];
+
+export const button = {
+  /** @type {import("@mlaursen/wc/types").ButtonSize[]} */
+  sizes: defaultExtraSizes,
+
+  /** @type {import("@mlaursen/wc/types").ButtonShape[]} */
+  shapes: ["round", "square"],
+
+  /** @type {import("@mlaursen/wc/types").ButtonVariant[]} */
+  variants: ["tonal", "text", "outlined", "filled", "elevated"],
+
+  playground: true,
+};
+
+export const eleventyComputed = {
+  playground: (data) => {
+    const slug = data.page.fileSlug;
+
+    return data[slug]?.playground ?? null;
+  },
+};
