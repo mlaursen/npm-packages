b30eb0cd06f5796b6da82278a1d1f3ca810aa815
Mikkel Laursen <mlaursen03@gmail.com>
2026-08-08 12:16:07 -0400

    fix(card): border-radius behavior

--- packages/wc/src/card/card.ts
+++ packages/wc/src/card/card.ts
@@ -7,6 +7,13 @@ import {
 import { customElement, property } from "lit/decorators.js";
 
 import { AriaMixin } from "../aria-mixin/aria-mixin.js";
+import type {
+  BoxAlignItems,
+  BoxGap,
+  BoxGrid,
+  BoxJustifyContent,
+  BoxPadding,
+} from "../box/types.js";
 import { InteractionMixin } from "../interaction/interaction-mixin.js";
 import { MarginMixin } from "../margin/margin-mixin.js";
 import { PaletteMixin } from "../palette/palette-mixin.js";
@@ -21,6 +28,46 @@ const BaseCard = AriaMixin(BaseStyledCard, "button");
 @customElement("mwc-card")
 export class Card extends BaseCard {
   static override styles = [...BaseCard.styles, styles];
+
+  /** Pass-through box property */
+  @property()
+  align: BoxAlignItems = "stretch";
+
+  /** Pass-through box property */
+  @property()
+  justify: BoxJustifyContent = "stretch";
+
+  /** Pass-through box property */
+  @property({ type: Boolean })
+  inline = false;
+
+  /** Pass-through box property */
+  @property()
+  grid: BoxGrid = false;
+
+  /** Pass-through box property */
+  @property({ type: Boolean, attribute: "full-width" })
+  fullWidth?: boolean;
+
+  /** Pass-through box property */
+  @property({ type: Boolean })
+  stacked = true;
+
+  /** Pass-through box property */
+  @property({ type: Boolean })
+  reversed?: boolean;
+
+  /** Pass-through box property */
+  @property()
+  gap: BoxGap = "none";
+
+  /** Pass-through box property */
+  @property({ type: Boolean })
+  nowrap?: boolean;
+
+  /** Pass-through box property */
+  @property()
+  padding: BoxPadding = "none";
 
   @property({ reflect: true })
   variant: CardVariant = "elevated";
@@ -61,9 +108,21 @@ export class Card extends BaseCard {
 
   override render(): TemplateResult {
     return html`
-      <slot name="header"></slot>
-      <slot></slot>
-      <slot name="footer"></slot>
+      <mwc-box
+        align=${this.align}
+        justify=${this.justify}
+        gap=${this.gap}
+        padding=${this.padding}
+        .grid=${this.grid}
+        .inline=${this.inline}
+        .nowrap=${this.nowrap}
+        .stacked=${this.stacked}
+        .reversed=${this.reversed}
+        .fullWidth=${this.fullWidth}
+        class="container"
+      >
+        <slot></slot>
+      </mwc-box>
       <mwc-elevation></mwc-elevation>
       ${this.#renderInteraction()}
     `;
