2b519b1331611649f2f407a242cb05bd4863c06c
Mikkel Laursen <mlaursen03@gmail.com>
2026-08-15 18:38:07 -0400

    build: allow oxfmt to format CHANGELOG

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   printWidth: 80,
   sortImports: true,
   ignorePatterns: [
-    "CHANGELOG.md",
-    "**/CHANGELOG.md",
     "**/cli.js",
     "packages/wc/src/**/*-styles.ts",
     "apps/docs/public",
