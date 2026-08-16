0117cb4ae85ef7fccedafc6d13257f57ff8f1e1f
Mikkel Laursen <mlaursen03@gmail.com>
2026-08-16 11:50:13 -0400

    build: fix last oxlint config

--- packages/wc/oxlint.config.ts
+++ packages/wc/oxlint.config.ts
@@ -1,16 +1,19 @@
-import { configs } from "@mlaursen/oxlint-config";
+import { createConfig } from "@mlaursen/oxlint-config";
 import { defineConfig } from "oxlint";
 
-export default defineConfig({
-  extends: [configs.recommended],
-  rules: {
-    "@typescript-eslint/no-empty-object-type": [
-      "error",
-      {
-        allowInterfaces: "never",
-        allowObjectTypes: "never",
-        allowWithName: ".+Overrides$",
+export default defineConfig(
+  createConfig({
+    overrides: {
+      rules: {
+        "@typescript-eslint/no-empty-object-type": [
+          "error",
+          {
+            allowInterfaces: "never",
+            allowObjectTypes: "never",
+            allowWithName: ".+Overrides$",
+          },
+        ],
       },
-    ],
-  },
-});
+    },
+  }),
+);
