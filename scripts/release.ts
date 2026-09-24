cfde3013922c895f0b8c2e15756b2819bfd265dd
Mikkel Laursen <mlaursen03@gmail.com>
2026-09-24 16:02:21 -0400

    added release notes since I always forget

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,5 +1,17 @@
 import { release } from "../packages/release-script/src/release.js";
 
+// Since I always forget:
+// 1. Make sure I am only logged in to npm
+//    - pnpm logout
+// 2. Either use npm login or the generic publish token
+//    - Generic Publish Token
+//      - `npm logout`
+//      - `echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc`
+//      - `nvim ~/.zshrc.local` to have `export NPM_TOKEN=......`
+//      - `reso`
+//    - Npm Login
+//      - `npm login`
+// 3. Publish hopefully works
 await release({
   repo: "npm-packages",
   buildCommand: "build-dist",
